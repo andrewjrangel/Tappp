938a7880bf6e2b60cda931da3df18db1c8ce317c
Andrew Rangel <andrewjrangel@gmail.com>
2013-03-25 18:05:10 -0500

    Initial Commit

---
+++ Tappp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tappp
+//
+//  Created by Ventura Rangel on 3/25/13.
+//  Copyright (c) 2013 Andrews Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
